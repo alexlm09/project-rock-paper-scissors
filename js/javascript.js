3f9ed38566e0f918eaef8e8dfdf0a3b57ddad8b2
alexlm09 <110418483+alexlm09@users.noreply.github.com>
2026-02-03 11:52:38 +0100

    Añadir funcion para generar numeros aleatorios entre 1 y 3

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+function randomNumber () {
+  let result;
+  let minNumber = 1;
+  let maxNumber = 3;
+
+  result = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+  return result;
+}
+
